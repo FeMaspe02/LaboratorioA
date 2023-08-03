d23d704a0eef231dd37699e919594b2ce78bcc99
FeMaspe02 <femaspe02@gmail.com>
2023-08-03 14:44:35 +0200

    .

--- ClimateMonitoring/src/LabA/Utente.java
+++ ClimateMonitoring/src/LabA/Utente.java
@@ -10,10 +10,18 @@ public class Utente {
 	private String userid;
 	private String password;
 	//centro di monitoraggio di afferenza
+	private FileManager fm = new FileManager();
 	
 	
-	private String fileSessione = "eh... bho";
-	private String fileUtenti = "eh.. bho";
-			
+	private String fileSessione = "/ClimateMonitoring/src/LabA/File/Sessione";
+	private String fileUtenti = "/ClimateMonitoring/src/LabA/File/Utenti";
+	
+	// svuota il file sessione. Viene chiamato all'inizio del programma, per fare si che il file sessione sia vuoto
+	//Viene riempito poi con i dati dell'utente che ha fatto login
+	
+	public Utente() {
+		PrintWriter pw = fm.openToWrite(fileSessione, false);
+		pw.print("");
+	}
 	
 }
