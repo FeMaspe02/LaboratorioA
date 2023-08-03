e6b61fd0ff62c1fb87f19f673578baaabc3c3d49
FeMaspe02 <femaspe02@gmail.com>
2023-08-03 14:28:21 +0200

    Aggiunta primi oggetti - file della lista utenti e sessione corrente, classe utente e classe FileManager

---
+++ ClimateMonitoring/src/LabA/FileManager.java
@@ -0,0 +1,42 @@
+package LabA;
+import java.io.*;
+
+public class FileManager {
+	
+	public FileManager() {
+		
+	}
+	
+	
+	//apre un file in scrittura 
+	
+	public PrintWriter openToWrite(String file, boolean append) {
+		
+		PrintWriter pw = null;
+		
+		try {
+			FileWriter fw = new FileWriter(file , append);
+			BufferedWriter bw = new BufferedWriter(fw);
+			pw = new PrintWriter(bw);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return pw;
+	}
+	
+	//apre un file in lettura
+	public BufferedReader openToRead(String file) {
+		BufferedReader br = null;
+		
+		try {
+			FileReader fr = new FileReader(new File(file));
+			br = new BufferedReader(fr);
+		}catch(IOException e ) {
+			e.printStackTrace();
+		}
+		
+		return br;
+	}
+
+}
---
+++ ClimateMonitoring/src/LabA/Utente.java
@@ -0,0 +1,19 @@
+package LabA;
+import java.io.*;
+import java.util.*;
+
+public class Utente {
+	private String nome;
+	private String cognome;
+	private String codiceFiscale;
+	private String postaElettronica;
+	private String userid;
+	private String password;
+	//centro di monitoraggio di afferenza
+	
+	
+	private String fileSessione = "eh... bho";
+	private String fileUtenti = "eh.. bho";
+			
+	
+}
