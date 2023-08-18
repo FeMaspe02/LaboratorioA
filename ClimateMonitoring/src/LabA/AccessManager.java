4021b45a7a58cec8e8852aaa27be6e1b49ad054f
david <david@DESKTOP-P6AKH9P.homenet.telecomitalia.it>
2023-08-18 18:49:46 +0200

    Ho fatto qualcosa decisamente, devo debuggare

--- ClimateMonitoring/src/LabA/AccessManager.java
+++ ClimateMonitoring/src/LabA/AccessManager.java
@@ -1,9 +1,11 @@
 package LabA;
 import java.io.*;
+import java.util.LinkedList;
 
 public class AccessManager {
 	
 	private String FileAree = "ClimateMonitoring/bin/LabA/File/CoordinateMonitoraggio.dati.CSV";
+	private String FileParametri = "ClimateMonitoring/bin/LabA/File/ParametriClimatici.dati.txt";
 	private FileManager fm = new FileManager();
 
 	public AccessManager() {
@@ -12,14 +14,18 @@ public class AccessManager {
 	
 	//Metodo che riceve in input una string che puo' essere un nome di un'area di interesse 
 	//o una parte di esso e restituisce TUTTE le aree di interesse che contengono data stringa nel loro nome
-	public void cercaAreaGeografica(String NomeParz) throws IOException {
-			
+	public LinkedList<AreaDiInteresse> cercaAreaGeografica(String NomeParz) throws IOException {
+		
+		AccessManager accesso = new AccessManager();
 		int Size = NomeParz.length();
 		String nomeparz = NomeParz.toLowerCase();
 		BufferedReader br = fm.openToRead(FileAree);
 		String Riga = br.readLine();
 		int c = 0;
-		
+		LinkedList<AreaDiInteresse> lista = new LinkedList<AreaDiInteresse>();
+		AreaDiInteresse a;
+		int count = 1;
+		int[] valori = new int[6];
 		do{		
 			
 			int i = 0;
@@ -29,32 +35,54 @@ public class AccessManager {
 			
 				String nome = Riga.substring(0,i).toLowerCase();
 				int n = ++i;
-
+				int k = n;
+				
 			while(Riga.charAt(i) != ';') {i++;}	
 			
 				String stato = Riga.substring(n,i).toLowerCase();
+				n = ++i;
+				
+			while(Riga.charAt(i) != ',') {i++;}
+			
+				double latitudine = Double.parseDouble(Riga.substring(n,i));
+				n = ++i;
+				
+				double longitudine = Double.parseDouble(Riga.substring(n,Riga.length()));
 				
 				if(stato.equals(nomeparz)) {
 					c++;
+					valori =  accesso.ottienivaloriparametri(count);
+					a = new AreaDiInteresse(nome,stato,latitudine,longitudine,valori[0],valori[1],valori[2],valori[3],valori[4],valori[5],valori[6]);
+					lista.add(a);
 					System.out.println(c + " " + nome + ", " + stato);
 					Trovato = true;
 					}
                
 				if(!Trovato) {
-					for (i = 0; i < n -1-Size; i++) {						
+					for (i = 0; i < k -1-Size; i++) {						
 							if (nome.substring(i + Size).equals(nomeparz)) {
+								valori =  accesso.ottienivaloriparametri(count);
+								a = new AreaDiInteresse(nome,stato,latitudine,longitudine,valori[0],valori[1],valori[2],valori[3],valori[4],valori[5],valori[6]);
 								System.out.println(c + " " + nome + ", " + stato);
+								lista.add(a);
 								c++;
 							}
 					}
 				}	
 				
 				Riga = br.readLine();
+				count ++;
 				
 				}while(Riga!=null);
 			if (c == 0) System.out.println("Non ci sono riscontri positivi per l'input immesso");
-			br.close(); 
+			br.close();
+			return lista; 
 	}	
+	
+	public AreaDiInteresse scegliAreaDiInteresseNome(String nome, int n) throws IOException {
+		LinkedList<AreaDiInteresse> lista = cercaAreaGeografica(nome);
+		return lista.get(n);
+	}
 	
 	//Metodo che riceve in ingresso latitudine e longitudine e restituisce l'area di interesse con date coordinate
 	
@@ -104,9 +132,41 @@ public class AccessManager {
 		br.close();
 	}
 	
-	public void visualizzaAreaGeografica() {
-		
+	public int[] ottienivaloriparametri(int n) throws IOException{
+		int[] valori = new int[6];
+		BufferedReader br = fm.openToRead(FileParametri);
+		String s = null;
+		for(int i = 0; i < n; i++) {
+			s = br.readLine();
+		}
+		int c = 0;
+		while(s.charAt(c) != ';') {c++;}
+		valori[0] = Integer.parseInt(s.substring(0,c));
+		while (s.charAt(c) != ';') {c++;}
+		int k = ++c;
+		while (s.charAt(c) != ';') {c++;}
+		valori[1] = Integer.parseInt(s.substring(n,c));
+		while (s.charAt(c) != ';') {c++;}
+		k = ++c;
+		while (s.charAt(c) != ';') {c++;}
+		valori[2] = Integer.parseInt(s.substring(n,c));
+		while (s.charAt(c) != ';') {c++;}
+		k = ++c;
+		while (s.charAt(c) != ';') {c++;}
+		valori[3] = Integer.parseInt(s.substring(n,c));
+		while (s.charAt(c) != ';') {c++;}
+		k = ++c;
+		while (s.charAt(c) != ';') {c++;}
+		valori[4] = Integer.parseInt(s.substring(n,c));
+		while (s.charAt(c) != ';') {c++;}
+		k = ++c;
+		while (s.charAt(c) != ';') {c++;}
+		valori[5] = Integer.parseInt(s.substring(n,c));
+		while (s.charAt(c) != ';') {c++;}
+		k = ++c;
+		while (s.charAt(c) != ';') {c++;}
+		valori[6] = Integer.parseInt(s.substring(n,c));
+		return valori;
 	}
-
 	
 }
---
+++ ClimateMonitoring/src/LabA/AreaDiInteresse.java
@@ -0,0 +1,43 @@
+package LabA;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class AreaDiInteresse {
+	
+	 private String nome;
+	 private String stato;
+	 private Double latitudine; 
+	 private Double longitudine;
+	 private int Vento;
+	 private int Umidità;
+	 private int Pressione;
+	 private int Temperatura;
+	 private int Precipitazioni;
+	 private int AltitudineDeiGhiacciai;
+	 private int MassaDeiGhiacciai;
+
+	 
+	 public AreaDiInteresse (String nome,String stato,Double latitudine,Double longitudine, int Vento, int Umidità, int Pressione, int Temperatura,int Precipitazioni,int AltitudineDeiGhiacciai,int MassaDeiGhiacciai) {
+		 this.nome = nome;
+		 this.stato = stato;
+		 this.latitudine = latitudine;
+		 this.longitudine = longitudine; 
+		 this.Vento = Vento;
+		 this.Umidità = Umidità;
+		 this.Pressione = Pressione;
+		 this.Temperatura = Temperatura;
+		 this.Precipitazioni = Precipitazioni;
+		 this.AltitudineDeiGhiacciai = AltitudineDeiGhiacciai;
+		 this.MassaDeiGhiacciai = MassaDeiGhiacciai;
+	 }
+	 
+	 
+	 public void visualizzaAreaGeografica(String nome, int n) throws IOException {
+		 AccessManager a = new AccessManager();
+			LinkedList<AreaDiInteresse> lista = a.cercaAreaGeografica(nome);
+			AreaDiInteresse area = lista.get(n);
+			System.out.print(area.Vento);
+	 }
+	 
+}
